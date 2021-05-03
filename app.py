f148b29e3353dce50a807cf91643b78a7be9583e
Francisco Leví <francisco.levi.md@gmail.com>
2021-05-03 14:34:55 -0500

    Add pokemon data.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pokemons_data import pokemons
 
 
@@ -23,6 +23,16 @@ def get_pokemon(name):
 			result = pokemon
 			return jsonify({"pokemon":pokemon})
 	return jsonify({"message":"Pokemon not found"})
+
+
+@app.route('/pokemons', methods=['POST'])
+def add_pokemon():
+	new_pokemon = { 
+		'name': request.json['name'], 
+		'url': request.json['url']
+	}
+	pokemons.append(new_pokemon)
+	return 	jsonify({'message':'Pokemon added succesfully.', 'pokemons':pokemons})
 
 
 if __name__ == '__main__':
