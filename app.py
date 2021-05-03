09f028da7495393c5e2f282f9bf83cf02d142e13
Francisco Leví <francisco.levi.md@gmail.com>
2021-05-03 14:09:01 -0500

    Get pokemon data.

--- app.py
+++ app.py
@@ -15,5 +15,15 @@ def get_pokemons():
 	return jsonify({"page":1, "pokemons":pokemons})
 
 
+@app.route('/pokemons/<string:name>')
+def get_pokemon(name):
+	result = {}
+	for pokemon in pokemons:
+		if pokemon.get('name') == name:
+			result = pokemon
+			return jsonify({"pokemon":pokemon})
+	return jsonify({"message":"Pokemon not found"})
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
